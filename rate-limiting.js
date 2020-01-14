06a3d6168195b966a08732828e0291f081cb2edf
Glitch (habitual-license) <none>
2020-01-14 16:17:22 +0000

    Redis-server added to ~/bin/ and working. \n Rate limiting adjusted and running.

--- rate-limiting.js
+++ rate-limiting.js
@@ -16,7 +16,8 @@ server.open((err) => {
 
 //---------------------
 module.exports = (req, res, next) => {
-  redisClient.exists(req.headers.user, (err, reply) => {
+  // redisClient.exists(req.headers.user, (err, reply) => {
+  redisClient.exists("user1", (err, reply) => {
     if (err) {
       console.log("Redis not working...")
       system.exit(0)
@@ -24,9 +25,11 @@ module.exports = (req, res, next) => {
     if (reply === 1) {
       // user exists
       // check time interval
-      redisClient.get(req.headers.user, (err, reply) => {
+      // redisClient.get(req.headers.user, (err, reply) => {
+      redisClient.get("user1", (err, reply) => {
         let data = JSON.parse(reply)
-        // console.log(">>>>>>>>>>>>>>>>>>>>>>>>", data)
+        // console. log(req.headers.user)
+        console.log(">>>>>>>>>>>>>>>>>>>>>>>>", data)
         let currentTime = moment().unix()
         let difference = (currentTime - data.startTime) / 60
         if (difference >= 1) {
@@ -34,28 +37,32 @@ module.exports = (req, res, next) => {
             'count': 1,
             'startTime': moment().unix()
           }
-          redisClient.set(req.headers.user, JSON.stringify(body))
+          // redisClient.set(req.headers.user, JSON.stringify(body))
+          redisClient.set("user1", JSON.stringify(body))
           // allow the request
           next()
         }
         if (difference < 1) {
-          if (data.count > 2) {
+          if (data.count > 15) {
             return res.json({ "error": 1, "message": "throttled limit exceeded..." })
           }
           // update the count and allow the request
           data.count++
-          redisClient.set(req.headers.user, JSON.stringify(data))
+          // redisClient.set(req.headers.user, JSON.stringify(data))
+          redisClient.set("user1", JSON.stringify(data))
           // allow request
           next()
         }
       })
     } else {
+      console.log("added new user")
       // add new user
       let body = {
         'count': 1,
         'startTime': moment().unix()
       }
-      redisClient.set(req.headers.user, JSON.stringify(body))
+      // redisClient.set(req.headers.user, JSON.stringify(body))
+      redisClient.set("user1", JSON.stringify(body))
       // allow request
       next()
     }
